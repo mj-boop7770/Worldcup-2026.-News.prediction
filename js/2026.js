0cf6618b38f42d98672a642768886ffc13f9ecae
mj-boop7770 <mrloverflv@gmail.com>
2026-06-19 20:51:03 +0200

    Update 2026.js

--- js/2026.js
+++ js/2026.js
@@ -1,12 +1,8 @@
 export async function chargerDonneesMondial() {
     try {
-        // On utilise l'URL complète du projet
         const url = 'https://mj-boop7770.github.io/Worldcup-2026.-News.prediction/2026.json';
         const response = await fetch(url);
-        
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP: ${response.status}`);
-        }
+        if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
         return await response.json();
     } catch (error) {
         console.error("❌ Impossible de charger le JSON :", error);
