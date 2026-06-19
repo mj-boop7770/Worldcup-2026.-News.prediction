580f5ba397e8f552d8e45c92a5e91c50f14b95d7
mj-boop7770 <mrloverflv@gmail.com>
2026-06-19 10:28:30 +0200

    Update mascotte.js

--- js/mascotte.js
+++ js/mascotte.js
@@ -1,1 +1,11 @@
-
+// js/mascotte.js
+export const Mascotte = {
+    afficher: (containerId) => {
+        const container = document.getElementById(containerId);
+        if (container) {
+            container.innerHTML = `
+                <img src="octopus.png" alt="Mujos Octopus" id="octopus-img" style="width: 150px;">
+            `;
+        }
+    }
+};
